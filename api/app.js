bc3fdfd464b95e0829e5ec4eb2d3251e943dc2dc
aibolt450 <aibolt450@gmail.com>
2025-06-25 21:32:00 +0300

    Виправлення підключення до API та запуск серверів

--- api/app.js
+++ api/app.js
@@ -20,6 +20,9 @@ app.use(
   cors({
     //client side   //admin side
     origin: ["http://localhost:3000", "http://localhost:3001"],
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization', 'token']
   })
 );
 
